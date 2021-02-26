c017ad7760b34f286dcd8a776c927a2a9e555ad1
Ted <60172917+ted-grankvist-nti-johanneberg@users.noreply.github.com>
2021-02-26 16:23:58 +0100

    Finished registration system
    -Added and basically finished registration a registation system
    -more model.rb functions for further use

--- app.rb
+++ app.rb
@@ -6,3 +6,40 @@ require_relative './model.rb'
 
 enable :sessions
 
+get('/') do
+    slim(:register)
+end
+  
+get('/showlogin') do
+    slim(:login)
+end
+
+post('/users/new') do
+    username = params[:username]
+    password = params[:password]
+    confirmpassword = params[:confirmpassword]
+    year_of_birth = params[:year_of_birth]
+    country = params[:country]
+    gender = params[:gender]
+    #Dock innan här, gör felhantering för att kolla om användaren redan finns, annars ger sinatra error, utan testa ifall den är empty typ och felhantera.
+    db = connect_to_db('db/forum2021.db')
+    result = check_registration(db, username, password, confirmpassword)
+    if result == "goodtogo" #Alltså om en användare med det inskriva namnet inte finns returnerar databasen nil, och då kan vi skapa en ny användare. Funktionen från model.rb returnerar 1,2 eller 3. 1 betyder good to go, skapa användare; 2 betyder att lösenorden ej matchar och 3 betyder att användaren redan finns.
+        #Lägg till user:
+        password_digest = BCrypt::Password.create(params[:password])
+        add_user(username, password_digest, year_of_birth, country, gender)
+        redirect('/')
+    elsif result == "wrongpass"
+        #Felhantering
+        content = "Your passwords don't match, please try again."
+        returnto = "/"
+        linktext = "Try again"
+        slim(:standarderror, locals:{content: content, returnto: returnto, linktext: linktext})
+    elsif result == "userexist"
+        #Felhantering
+        content = "This user already exists, try another username."
+        returnto = "/"
+        linktext = "Try again"
+        slim(:standarderror, locals:{content: content, returnto: returnto, linktext: linktext})
+    end
+end
--- model.rb
+++ model.rb
@@ -3,3 +3,23 @@ def connect_to_db(path)
     db.results_as_hash = true
     return db
 end
+
+def check_registration(db, username, password, confirmpassword)
+    result = db.execute("SELECT * FROM users WHERE username=?", username).first
+    if result == nil
+        if password == confirmpassword
+            return "goodtogo" #Good to go, bara att skapa användaren.
+        else
+            return "wrongpass" #Lösenorden matchar inte
+        end
+    else
+        return "userexist" #Användaren finns redan
+    end
+end
+
+def add_user(username, password_digest, year_of_birth, country, gender)
+    usertype = "user" #När vi skapar en användare blir den en vanlig användare, admin-behörigheter kan erhållas senare.
+    db = connect_to_db('db/forum2021.db')
+    db.execute('INSERT INTO users (username, pwdigest, country, year_of_birth, gender, usertype) VALUES (?,?,?,?,?,?)',username,password_digest,country,year_of_birth,gender,usertype)
+    return true
+end
